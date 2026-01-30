ad2e5cfe0e7a0c5dac7a0be7e591ac8d785df1db
Milind1234 <milindchavan525@gmail.com>
2026-01-30 13:08:25 +0530

    #

--- others/inbuilt_ds_Practice_Question/print_name_for_N_times_using_Recursion.py
+++
@@ -1,7 +0,0 @@
-def Print_name(n):
-    if n == 0:
-        return
-    print("XYZ")
-    Print_name(n-1)
-    
-Print_name(5)
--- others/practice_python/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-def changeName(obj,name):
-        #         self.__class__.name = "Rahul"
-
-        # @classmethod
-        # def changeName(cls, name):
-        #         cls.name = name
