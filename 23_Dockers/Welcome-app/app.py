5b3658f93fb7d069c73eafabe3091a59a90e0cce
Milind1234 <milindchavan525@gmail.com>
2026-07-12 00:21:39 +0530

    docker

--- 23_Dockers/Welcome-app/app.py
+++ 23_Dockers/Welcome-app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-import os 
+import os
+
 app = Flask(__name__)
 
-@app.route("/",methods=['Get'])
+@app.route("/", methods=["GET"])
 def home():
     return "Hello World"
 
-if __name__ == "__main__":   
-    app.run(debug=True,host= "0.0.0.0" ,port = )
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
