83879dc12dbe38f96c0438acf71207188b67b8d7
Milind1234 <milindchavan525@gmail.com>
2026-07-13 18:35:03 +0530

    Logging and exception

---
+++ 25_End_to_end_ML_Project/src/logger.py
@@ -0,0 +1,21 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+# Create logs directory
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+# Full log file path
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler(LOG_FILE_PATH),
+        logging.StreamHandler()
+    ]
+)
